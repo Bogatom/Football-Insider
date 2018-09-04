3511baeb6281c2888cad5f07433d1ea5a35dc9c8
Tom van den Bogaart <bogatom98@gmail.com>
2018-09-04 12:08:22 +0200

    aanroepen van scripts en css vereenvoudigd
    beter aanroepen

--- Football-Insider/App_Start/BundleConfig.cs
+++ Football-Insider/App_Start/BundleConfig.cs
@@ -21,11 +21,12 @@ namespace Football_Insider
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/ScrollScript.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
