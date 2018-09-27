cb0a79d21d239d71909010362d6a314d5b1a8410
Tom van den Bogaart <bogatom98@gmail.com>
2018-09-27 15:19:52 +0200

    form toegevoegd

--- MDL/Article.cs
+++ MDL/Article.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MDL
 {
@@ -7,9 +8,17 @@ namespace MDL
     {
         #region Properties
         public int ArticleId { get; set; }
+
+        [Required(ErrorMessage = "Voer een titel in.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Voer een categorie in.")]
         public string Category { get; set; }
+
+        [Required(ErrorMessage = "Voeg een beschrijving toe.")]
         public string Content { get; set; }
+
+        [Required(ErrorMessage = "Voeg een afbeelding(en) toe.")]
         public List<string> Image { get; set; }
         #endregion 
     }
