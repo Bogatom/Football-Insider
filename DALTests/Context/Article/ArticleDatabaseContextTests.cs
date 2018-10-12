e0768069d6fe5dd25cefbd906399d32c1dad6250
Tom van den Bogaart <bogatom98@gmail.com>
2018-10-12 13:04:43 +0200

    Unit testen bijgewerkt

--- DALTests/Context/Article/ArticleDatabaseContextTests.cs
+++ DALTests/Context/Article/ArticleDatabaseContextTests.cs
@@ -16,23 +16,124 @@ namespace DAL.Contexts.Tests
         [TestMethod()]
         public void GetAllArticlesTest()
         {
-            //arrange
+            //Arrange
             IArticleContext context = new ArticleDatabaseContext();
             ArticleRepository repo = new ArticleRepository(context);
             ArticleLogic logic = new ArticleLogic(repo);
 
             List<Article> AllArticles = logic.GetAllArticles();
 
-            //act
+            //Act
             int expected = 3;
             int actual = AllArticles.Count();
 
-            //assert
+            //Assert
             Assert.AreEqual(expected, actual);
+        }
 
+        [TestMethod()]
+        public void AddArticle()
+        {
+            IArticleContext context = new ArticleDatabaseContext();
+            ArticleRepository repo = new ArticleRepository(context);
+            ArticleLogic logic = new ArticleLogic(repo);
 
-            //todo: meer unit test maken
+            //Arrange
+            BindModel article = new BindModel();
+            List<Article> Articles = new List<Article>();
+            Articles.Add(article._Article);
 
+            //Act
+            int expected = 1;
+            int actual = Articles.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetAllCategories()
+        {
+            //Arrange
+            IArticleContext context = new ArticleDatabaseContext();
+            ArticleRepository repo = new ArticleRepository(context);
+            ArticleLogic logic = new ArticleLogic(repo);
+
+            List<Category> AllCategories = logic.GetAllCategories();
+
+            //Act
+            int expected = 5
+;
+            int actual = AllCategories.Count();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void AddCategoryToArticle()
+        {
+            //Arrange
+            IArticleContext context = new ArticleDatabaseContext();
+            ArticleRepository repo = new ArticleRepository(context);
+            ArticleLogic logic = new ArticleLogic(repo);
+
+            Category category = new Category();
+            category = logic.AddCategoryToArticle(4, "Premier League");
+
+            Category categoryTwo = new Category();
+            categoryTwo = logic.AddCategoryToArticle(0, "");
+
+            //Act
+            string expected = category.CategoryName = "Premier League";
+            string actual = categoryTwo.CategoryName = "";
+
+            //Assert
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetCurrentArticle()
+        {
+            //Arrange
+            IArticleContext context = new ArticleDatabaseContext();
+            ArticleRepository repo = new ArticleRepository(context);
+            ArticleLogic logic = new ArticleLogic(repo);
+
+            List<Article> Articles = new List<Article>();
+
+            Article article = new Article();
+            article = logic.GetCurrentArticle(117);
+            Articles.Add(article);
+
+            //Act
+            int actual = Articles.Count;
+            int expected = 1;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void EditArticle()
+        {
+            //Arrange
+            IArticleContext context = new ArticleDatabaseContext();
+            ArticleRepository repo = new ArticleRepository(context);
+            ArticleLogic logic = new ArticleLogic(repo);
+
+            BindModel article = new BindModel();
+            Article oldContent = logic.GetCurrentArticle(117);
+
+            string oldArticleContent = oldContent.Content;
+            string newArticleContent = "BLABLABLABLAB";
+
+            //Act
+            string actual = oldArticleContent;
+            string expected = newArticleContent;
+
+            //Assert
+            Assert.AreNotEqual(expected, actual);
         }
     }
 }
